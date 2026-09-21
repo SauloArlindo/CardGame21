9f0201991d115518450795bfc1c6a13be9e29aec
SauloMota <sauloarlindopereirapereira@gmail.com>
2026-09-20 22:33:04 -0300

    Finalizado o fluxo de partida

--- src/main/games/Card21.java
+++ src/main/games/Card21.java
@@ -1,11 +1,11 @@
 package main.games;
 
-import java.util.List;
 import java.util.Scanner;
 import main.entities.Baralho;
-import main.entities.Carta;
 import main.entities.Jogador;
 import main.entities.Maquina;
+import main.enums.Decisao;
+
 
 public class Card21 {
 
@@ -29,9 +29,50 @@ public class Card21 {
             maquina.addCartaMao(baralho.RetirarCarta());
         }
     }
-    public List<Carta> mostrarCartasPlayer(){
-        return player.getCartasMao();
+    public void mostrarCartasPlayer(){
+        System.out.println("Suas Cartas :" + player.getCartasMao());
+        System.out.println("Pontos:"+ player.calcularPonto());
+    }
+    public void turnoJogador(){
+        int escolha = 1;
+        while (escolha != 2){
+            mostrarCartasPlayer();
+            if (player.calcularPonto() < 21){
+                System.out.println("Deseja Comprar uma Carta:  1 para comprare 2 para terminar o turno");
+                escolha = sc.nextInt();
+                if (escolha == 1){
+                    player.addCartaMao(baralho.RetirarCarta());
+                }
+            }else{
+                escolha = 2;
+                System.out.println("Chegou ou ultrapassou o limite");
+            }
+        }
+    }
+    public void turnoMaquina(){
+        while(maquina.decidirJogada() == Decisao.COMPRAR){
+                maquina.addCartaMao(baralho.RetirarCarta());
+        }
+    }
+    public void resultadoPartida(){
+        int pontoplayer = player.calcularPonto();
+        int pontomaquina = maquina.calcularPonto();
+        System.out.println("Cartas Maquina :" + maquina.getCartasMao() +"\n Pontuação :" + pontomaquina);
+        System.out.println("Cartas Player :" + player.getCartasMao() +"\n Pontuação :" + pontoplayer);
         
+        String resultado = " ";
+        if (pontoplayer > 21) {
+            resultado = "Jogador Estourou Derrota";
+        }else if(pontomaquina > 21){
+            resultado = "Maquina Estourou Jogador Venceu";
+        }else if(pontomaquina > pontoplayer){
+            resultado = "Maquina Venceu";
+        }else if(pontomaquina < pontoplayer){
+            resultado = "Jogador Venceu";
+        }else if(pontomaquina == pontoplayer){
+            resultado =  "Empate";
+        }
+        System.out.println(resultado);
     }
 
 }
